098ff2e51923a5184c8742a1f420359f2276ef2a
Jose Manuel <patinogutierrez@icloud.com>
2016-01-31 17:47:49 -0600

    Tarea Terminada

--- Tarea_Juego_Memoria.playground/Contents.swift
+++ Tarea_Juego_Memoria.playground/Contents.swift
@@ -1,3 +1,26 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+
+//Generar un rango de 0 a 100
+for var i : (Int) = 0 ; i <= 100 ; i++ {
+    //4ª evaluación rango entre 30 y 40
+    switch i {
+    case 30 ... 40:
+        print("[\(i)]\t\t\t\tViva Swift!!")
+    default:
+        break
+    }
+    //1ª evaluación número divisible entre 5
+    if (i % 5 == 0){
+        print("[\(i)]\tBingo!")
+    }
+    //2ª evalaución número par
+    if(i % 2 == 0){
+        print("[\(i)]\t\tPar!!")
+    //3ª evaluación número impar
+    }else{
+        print("[\(i)]\t\t\tImpar!!")
+    }
+}
+
