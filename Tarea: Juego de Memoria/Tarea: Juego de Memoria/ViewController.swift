aedda3c96f41cf9424d8c865cb7c4b9df84f874b
Jose Manuel <patinogutierrez@icloud.com>
2016-01-31 16:00:34 -0600

    Create a project

---
+++ Tarea: Juego de Memoria/Tarea: Juego de Memoria/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarea: Juego de Memoria
+//
+//  Created by José on 31/01/16.
+//  Copyright © 2016 José. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
